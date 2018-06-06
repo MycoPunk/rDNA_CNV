742470e8b7e2139bc88d14c2d4708085cb4a174d
MycoPunk <34922294+MycoPunk@users.noreply.github.com>
2018-06-06 13:11:22 -0500

    Update pbio_to_Ill.R

--- pbio_to_Ill.R
+++ pbio_to_Ill.R
@@ -1,8 +1,12 @@
+
+#everything old is new again backup 
+
 #This script takes in PacBio .fastq consessus files and chops them into illumina-like 101bp reads
 #whcih can then be used in read depth analysis. This creates paired-end (-1, -2) reads with 2X coverage of each fragment. 
 
 
 .libPaths(new='~/R/x86_64-pc-linux-gnu-library/3.2')
+#setwd("~/Desktop")
 options(stringsAsFactors = FALSE)
 library(doParallel)
 library(foreach)
@@ -19,23 +23,27 @@ enableJIT(3)
 # Create cluster with desired number of cores
 cl <- makeCluster(20)
 
+clusterEvalQ(cl, .libPaths(new='~/R/x86_64-pc-linux-gnu-library/3.2'))
+
 # Register cluster
 registerDoParallel(cl)
 
 #set path and read in the PacBio .fastq
-P_bio_seqs <- data.table::fread("pbio-1487.13649.fastq", header = FALSE, quote="", sep="}")
+P_bio_seqs <- data.table::fread("corrected_pbio-a.fastq", header = FALSE, quote="", sep="}")
 
-#transpose into dataframe
+#P_bio_seqs <- data.table::fread("mock_pbio_reads.fastq", header = FALSE, quote="", sep="}")
+
+#transpose 
 Seq <- P_bio_seqs[seq(2, nrow(P_bio_seqs), 4), ]
 q_score <- P_bio_seqs[seq(4, nrow(P_bio_seqs), 4), ]
 PBIO_df<- data.frame(Seq, q_score)
 
-#get number of draws for ea. fragment
+#1: get number of draws for ea. fragment
 frag_length<- 500 
-PBIO_df<- data.table(PBIO_df, (round((nchar(PBIO_df[,1]) / frag_length) * .2, digits = 0)))
+PBIO_df<- data.table(PBIO_df, (round((nchar(PBIO_df[,1]) / frag_length) * 3, digits = 0)))
 colnames(PBIO_df) <- c("BP", "q-score", "num_draws")
 
-#function to take n 500 bp draws based on length of PacBio fragment  
+#function to take n 500 bp draws based on length of fragment  
 Fragment = function(string, n) {
   replicate(n= n,  {nStart <- sample(1:(nchar(string) -500), 1)
   samp <- substr(string, nStart, nStart + 499)
@@ -43,19 +51,21 @@ Fragment = function(string, n) {
   
 }
 
-#draw a different number 500 bp draws based on length of PacBio fragment  
+
+#2: draw a different number of 500 pb samples based on length of ea fragment 
 draws<- apply(PBIO_df, 1, function(x) data.frame(lapply(x[1:2], Fragment, n = x[3])))
 draws.df<- rbindlist(draws)
 
 
-#function to make unique id line
+#3: create function to make unique id line
 Header_ID = function(){
   header = c("@HISEQ15:100:AXXXXXXXX:1:1000:", sample(1000:2000, 1), ":", sample(1000:2000, 1), " ", "1:N:0:XXXXXX")
   paste(header, collapse="")
 }
 
 
-#function to assemble fastq files using above functions
+
+#4: assemble fastq file:
 make_fastq <- function(df) { 
   forward.read  <- NULL 
   reverse.read  <- NULL
@@ -78,26 +88,33 @@ make_fastq <- function(df) {
     reverse_header <- gsub("1:N:0", "2:N:0", header)
     forward.read[i] <- cat(header, first.101[i], x2, first.q[i], sep = "\n")
     reverse.read[i] <- cat(reverse_header, reverse[i], x2, reverse.q[i], sep = "\n")
-  }
-  return(forward.read)
-  return(reverse.read)
-}
+  }}
 
 
-#cmp for speed
 make_fastq_comp <- cmpfun(make_fastq)
 
-#split the table into subtables to increase output speed
-subtable <- 100
-rows <- nrow(draws.df)
-groups  <- rep(1:ceiling(rows/subtable),each=subtable)[1:rows]
-split_table <- split(draws.df,groups)
+#Print the new Illumina-style reads 
+#foreach(x=(capture.output(apply(draws.df, 1, function(x) make_fastq_comp(df = draws.df)), file = "Illumina_type_Pbio_reads_capture.output.txt", append = TRUE))) %dopar% {x}
 
 
-#capture the output of each subtable 
-for(i in 1:length(split_table)){
-  captureOutput(apply(split_table[[i]], 1, function(x) make_fastq_comp(df = (split_table[[i]]))), file = "Illumina_type_Pbio_reads.fasta", append = TRUE)
+chunk <- 100
+#chunk <- 100
+n <- nrow(draws.df)
+r  <- rep(1:ceiling(n/chunk),each=chunk)[1:n]
+d <- split(draws.df,r)
+#access each sub data.table like this:
+#df1 <- as.data.frame(d[[1]])
+
+
+#split print
+for(i in 1:length(d)){
+  captureOutput(lapply(d[i], function(x) make_fastq_comp(df = d[[i]])), 
+                file = "PRINT_WITH_SPLIT_from_3_a.fastq", append = TRUE)
 }
 
+
+#print without split 
+#captureOutput(make_fastq_comp(df = draws.df), 
+#              file = "PRINT_WITHOUT_split_from_a.fastq", append = TRUE)
 
 quit()
