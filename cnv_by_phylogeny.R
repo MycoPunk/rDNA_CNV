59497bf8987666e3f7e4b3ec3a30462cf13ff6a9
Lotus Lofgren <LLofgren@umn.edu>
2018-11-21 19:21:21 -0600

    Update cnv_by_phylogeny.R

--- cnv_by_phylogeny.R
+++ cnv_by_phylogeny.R
@@ -1,6 +1,5 @@
 #set directory
-#built from tutorial http://www.francoiskeck.fr/phylosignal/demo_plots.html
-setwd("~/Desktop/Project_ITS_CNV/CNV_Tree_NEW")
+setwd("")
 
 #set libraries
 library(phylosignal)
@@ -113,3 +112,44 @@ dotplot.phylo4d(local.i, dot.col = points.col, main = "no_outlier",
                 trait.cex = .8, 
                 tip.cex = .3, 
                 dot.cex = 1.5)
+
+
+####caper PGLS model
+#set libraries
+library(phylosignal)
+library(adephylo)
+library(ape)
+library(phylobase)
+library(caper)
+
+##read in file
+CNV_df<- read.csv("NEXUS_for_phylosignal_small.csv")
+View(CNV_df)
+
+#normallize genome size by the size of rDNA cassette 
+cassette_size_in_bp<- CNV_df$rDNA_CN*9100 #assume 9.1kb for average single cassette size
+genome_size_in_bp<- CNV_df$size*1000000 # Mbp convert to bp
+genome_length_w_cassette<- genome_size_in_bp + cassette_size_in_bp #add the casette size to the genome size
+df_with_rDNA_size1<-cbind(CNV_df, genome_size_in_bp)
+df_with_rDNA_size<-cbind(df_with_rDNA_size1, genome_length_w_cassette)
+
+#read in the tree
+phy<- as.character(CNV_df[1,3])
+phy<- read.tree(text = phy)
+
+#check the data
+length(phy$tip.label)
+
+#read in the data
+dat_WO_CN_length <- data.frame(taxa=df_with_rDNA_size$X, genome_size=df_with_rDNA_size$genome_size_in_bp, CNV=as.numeric(df_with_rDNA_size$rDNA_CN))
+dat_W_CN_length <- data.frame(taxa=df_with_rDNA_size$X, genome_size=df_with_rDNA_size$genome_length_w_cassette, CNV=as.numeric(df_with_rDNA_size$rDNA_CN))
+
+#match data to tips
+cdat_WO_CN<- comparative.data(data=dat_WO_CN_length, phy=phy, names.col="taxa", vcv=TRUE)
+cdat_W_CN<- comparative.data(data=dat_W_CN_length, phy=phy, names.col="taxa", vcv=TRUE)
+
+#pgls
+mod_WO <- pgls(formula = log(CNV) ~ log(genome_size), data = cdat_WO_CN)
+summary(mod_WO)
+mod_W <- pgls(formula= log(CNV) ~ log(genome_size), data = cdat_W_CN)
+summary(mod_W)
